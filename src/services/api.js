dabacf2d97bf83978a34dfd40330d9c98ab85557
Sahiti Akella <akellasahiti12@gmail.com>
2026-05-23 19:51:27 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://task-manager-backend-vjl0.onrender.com';
+const API_BASE_URL = 'https://task-manager-backend-vjl0.onrender.com'/api;
 
 const getHeaders = () => {
   const headers = {
