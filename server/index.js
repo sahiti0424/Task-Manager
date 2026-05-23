a5cf8644f80c622e2069d1755f4807b4c40d3845
Sahiti Akella <akellasahiti12@gmail.com>
2026-05-23 18:59:26 +0530

    cors all

--- server/index.js
+++ server/index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://task-manager-five-ruddy-32.vercel.app'
-  ],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
