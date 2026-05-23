4c83c5ab0f3b546f71e835191c14d7a597c77b6f
Sahiti Akella <akellasahiti12@gmail.com>
2026-05-23 18:34:26 +0530

    fixed railway env variables

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'student_management_db',
+  host: process.env.MYSQLHOST,
+port: process.env.MYSQLPORT,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
